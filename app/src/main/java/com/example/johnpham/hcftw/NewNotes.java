f1deb596330d4689affe55a5ef1c86e9a4ad3848
john098 <phamhv01@students.ipfw.edu>
2014-12-09 20:57:36 -0500

    Revert "adding logout"
    This reverts commit 26e0815308af9cd77e7e75a8ca21280e61aab757.

--- app/src/main/java/com/example/johnpham/hcftw/NewNotes.java
+++ app/src/main/java/com/example/johnpham/hcftw/NewNotes.java
@@ -2,7 +2,6 @@ package com.example.johnpham.hcftw;
 
 import android.app.Activity;
 import android.app.TimePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -158,11 +157,8 @@ public void call()
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.logout) {
-          Intent intent=new Intent(getApplicationContext(),Login.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-          startActivity(intent);
-          return true;
+        if (id == R.id.action_settings) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
